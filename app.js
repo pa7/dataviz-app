0e12fa9c9b15ad14f2a603f03434010420d28c8c
Patrick Wied <patrick.wied@live.at>
2014-03-25 22:12:53 +0100

    Added dependency injection for services

--- app.js
+++ app.js
@@ -5,12 +5,14 @@
    angular.module('datavizApp', [
       'datavizApp.directives',
       'datavizApp.controllers',
-      'vr.directives.slider'
+      'datavizApp.services',
+      'vr.directives.slider',
+      'ui.bootstrap'
    ]);
 
    // setup dependency injection
    angular.module('d3', []);
    angular.module('datavizApp.directives', ['d3']);
    angular.module('datavizApp.controllers', []);
-
+   angular.module('datavizApp.services', []);
 }());
